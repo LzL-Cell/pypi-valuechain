58113565881ea189589f53498c93b2d6c929ce1f
LzL-Cell <1367814387@qq.com>
2025-12-15 21:10:20 +0800

    Update joern_runner.sh

--- joern_runner.sh
+++ joern_runner.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
+JOERN=/home/lzl/bin/joern/joern-cli/joern
 
-JOERN=~/bin/joern/joern-cli/joern
-PYSRC2CPG=~/joern/joern-cli/frontends/pysrc2cpg/target/universal/stage/bin/pysrc2cpg
-
-mkdir -p cpgs results
+mkdir -p data cpgs
 
 for pkg in sources/*; do
   NAME=$(basename "$pkg")
   SRC="$pkg/src"
-  CPG="cpgs/$NAME.cpg.bin"
+  OUT="cpgs/$NAME.cpg.bin"
 
   if [ -d "$SRC" ]; then
-    echo "[CPG] Generating $NAME"
-    $PYSRC2CPG "$SRC" -o "$CPG"
+    echo "[Joern] Building CPG for $NAME"
+    ./frontends/pysrc2cpg/target/universal/stage/bin/pysrc2cpg \
+      "$SRC" -o "$OUT"
 
-    echo "[Joern] Query $NAME"
-    $JOERN --import "$CPG" --script joern_query.sc \
-      --params pkg="$NAME" > "results/$NAME.csv"
+    echo "[Joern] Exporting data for $NAME"
+    $JOERN --load "$OUT" joern_export.sc
   fi
 done
 
